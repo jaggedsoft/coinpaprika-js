0b4f09e7f49f5cd14f8c364e9a40fd2a7e2f4c3c
Jon Eyrick <social@nextlocal.net>
2019-01-23 21:12:42 +0100

    Initial import

---
+++ coinpaprika.js
@@ -0,0 +1,108 @@
+( async () => {
+    const axios = require( 'axios' );
+    const instance = axios.create( {
+        baseURL: 'https://api.coinpaprika.com/v1',
+        headers: {
+            'User-Agent': 'coinpaprika-js'
+        },
+        timeout: 30000
+    } );
+    async function request( url, params = {} ) {
+        return new Promise( ( resolve, reject ) => {
+            instance.get( url, { params } )
+                .then( function ( response ) {
+                    resolve( response.data );
+                } )
+                .catch( function ( error ) {
+                    if ( error.response ) console.warn( error.response.data );
+                    throw error.message;
+                } );
+        } );
+    }
+
+    // Get market overview data
+    exports.global = async ( params = {} ) => {
+        return request( '/global', params );
+    };
+
+    // List coins
+    exports.coins = async ( params = {} ) => {
+        return request( '/coins', params );
+    };
+
+    // Get coin by ID
+    exports.coin = async ( id = "btc-bitcoin", params = {} ) => {
+        return request( `/coins/${id}`, params );
+    };
+
+    // Get markets by coin ID (USD,BTC,ETH,PLN)
+    exports.markets = async ( id = "btc-bitcoin", params = { quotes: "USD" } ) => {
+        return request( `/coins/${id}/markets`, params );
+    };
+
+    // Get exchanges by coin ID
+    exports.exchanges = async ( id = "btc-bitcoin", params = {} ) => {
+        return request( `/coins/${id}/exchanges`, params );
+    };
+
+    // Get events by coin ID
+    exports.events = async ( id = "btc-bitcoin", params = {} ) => {
+        return request( `/coins/${id}/events`, params );
+    };
+
+    // Get tweets by coin ID
+    exports.twitter = async ( id = "btc-bitcoin", params = {} ) => {
+        return request( `/coins/${id}/twitter`, params );
+    };
+
+    // Get tickers for all coins (USD, BTC, ETH)
+    exports.tickers = async ( params = { quotes: "USD" } ) => {
+        return request( '/tickers', params );
+    };
+
+    // Get ticker information for a specific coin (USD, BTC, ETH)
+    exports.ticker = async ( id, params = { quotes: "USD" } ) => {
+        return request( `/coins/${id}`, params );
+    };
+
+    // Get historical ticker information for a specific coin (USD, BTC, ETH)
+    exports.historical = async ( id, start, params = { end: false, interval: "5m", quote: "USD", limit: 1000 } ) => {
+        params.start = start;
+        if ( typeof params.end == "undefined" || !params.end ) params.end = new Date().toISOString();
+        return request( `/tickers/${id}/historical`, params );
+    };
+
+    // List exchanges (USD,BTC,ETH,PLN)
+    exports.exchangeList = async ( params = { quotes: "USD" } ) => {
+        return request( "/exchanges", params );
+    };
+
+    // Get exchange by ID (USD,BTC)
+    exports.exchange = async ( id, params = { quotes: "USD" } ) => {
+        return request( `/exchanges/${id}`, params );
+    };
+
+    // Get markets by exchange ID (USD,BTC,ETH,PLN)
+    exports.exchangeMarkets = async ( id, params = { quotes: "USD" } ) => {
+        return request( `/exchanges/${id}/markets`, params );
+    };
+
+    // Search
+    exports.search = async ( query = "btc", params = { limit: 6, c: "currencies,exchanges,icos,people,tags" } ) => {
+        params.q = query;
+        return request( `/search`, params );
+    };
+
+    // Get 24h OHLC candle (USD, BTC)
+    exports.OHLCVLatest = async ( id = "btc-bitcoin", params = { quote: "USD" } ) => {
+        return request( `/coins/${id}/ohlcv/latest/`, params );
+    };
+
+    // Get Today's OHLC candle: rolls over at 23:59:59 (USD, BTC)
+    exports.OHLCVToday = async ( id = "btc-bitcoin", params = { quote: "USD" } ) => {
+        return request( `/coins/${id}/ohlcv/today/`, params );
+    };
+
+    // Tags https://api.coinpaprika.com/#tag/Tags
+    // People https://api.coinpaprika.com/#tag/People
+} )();
